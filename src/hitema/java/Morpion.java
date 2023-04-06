4b5bc4ee012fa3eea143332bd5d701e9ff6da98e
Annabelle <annabelle.bosse.pro@gmail.com>
2023-04-06 17:11:12 +0200

    ok
    pk

---
+++ src/hitema/java/Morpion.java
@@ -0,0 +1,114 @@
+package hitema.java;
+import java.util.Scanner;
+import java.util.*;
+
+import static hitema.java.domain.CheckWinner.checkWinner;
+import static hitema.java.modeles.PrintBoard.printBoard;
+
+public class Morpion {
+
+    public static String[] board;
+    public static String turn;
+    static String player1;
+    static String player2;
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        boolean playAgain = true;
+
+        while(playAgain){
+            board = new String[9];
+            turn = "X";
+            String winner = null;
+
+            for (int a = 0; a < 9; a++) {
+                board[a] = String.valueOf(a + 1);
+            }
+
+            System.out.println("Bienvenue sur le morpion");
+            System.out.println("Entrer le Player 1:");
+            player1 = in.nextLine();
+            System.out.println("Entrer le Player 2:");
+            player2 = in.nextLine();
+            System.out.println(player1 + " sera le x, et " + player2 + " sera le o.");
+            printBoard();
+
+
+
+            System.out.println(player1 + " commence. Veuillez saisir un numéro d'emplacement pour placer x:");
+
+            while (winner == null) {
+                int numInput;
+
+                // Exception handling.
+                // numInput will take input from user like from 1 to 9.
+                // If it is not in range from 1 to 9.
+                // then it will show you an error "Invalid input."
+                try {
+                    numInput = in.nextInt();
+                    if (!(numInput > 0 && numInput <= 9)) {
+                        System.out.println(
+                                "Invalide ; re-entrer un numéro:");
+                        continue;
+                    }
+                }
+                catch (InputMismatchException e) {
+                    System.out.println(
+                            "Invalide ; re-entrer un numéro:");
+                    continue;
+                }
+
+                // This game has two player x and O.
+                // Here is the logic to decide the turn.
+                if (board[numInput - 1].equals(
+                        String.valueOf(numInput))) {
+                    board[numInput - 1] = turn;
+
+                    if (turn.equals("X")) {
+                        turn = "O";
+                    }
+                    else {
+                        turn = "X";
+                    }
+
+                    printBoard();
+                    winner = checkWinner();
+                }
+                else {
+                    System.out.println(
+                            " emplacement indisponible; re-entrer un numéro:");
+                }
+            }
+
+            // If no one win or lose from both player x and O.
+            // then here is the logic to print "draw".
+            if (winner.equalsIgnoreCase("égalité")) {
+                System.out.println(
+                        " égalité! Merci d'avoir joué.");
+            }
+
+            // For winner -to display Congratulations! message.
+            else {
+                System.out.println(
+                        "Félicitation! " + winner
+                                + " a gagné! Merci d'avoir joué.");
+            }
+
+            //ask user if they want to play again
+            System.out.println("Voulez-vous jouer à nouveau? (o/n)");
+            String playAgainAnswer = in.next();
+            if(playAgainAnswer.equalsIgnoreCase("n")){
+                playAgain = false;
+                System.out.println("Ok Bye !!");
+            } else {
+                playAgain = true;
+            }
+
+            //clear scanner input buffer
+            in.nextLine();
+        }
+
+        in.close();
+    }
+}
---
+++ src/hitema/java/modeles/PrintBoard.java
@@ -0,0 +1,22 @@
+package hitema.java.modeles;
+
+import static hitema.java.Morpion.board;
+
+public class PrintBoard {
+
+    public static void printBoard() {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                + board[1] + " | " + board[2]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                + board[4] + " | " + board[5]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                + board[7] + " | " + board[8]
+                + " |");
+        System.out.println("|---|---|---|");
+    }
+}
